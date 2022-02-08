35895bdc5b0c68f93217d2db54dc3ef9861dfe0c
mastroalex <76497903+mastroalex@users.noreply.github.com>
2022-02-08 20:27:15 +0100

    ok

--- code/mySegmentation.m
+++ code/mySegmentation.m
@@ -6,56 +6,49 @@ clear all; close all;
 
 % set current file path 
 filePath = matlab.desktop.editor.getActiveFilename;
-pathDivided=strsplit(filePath,'/');
+pathDivided=strsplit(filePath,'\');
 newPath=erase(filePath,pathDivided(end));
-dataPath=strcat(newPath,'/dataset');
+dataPath=strcat(newPath,'dataset');
 
-addpath(strcat(newPath,'/functions')); %set path for functions
+addpath(strcat(newPath,'functions')); %set path for functions
 
 % set folder with net syntax
-imds = imageDatastore(strcat(newPath,'dataset/FRAME_TRAIN')); % link for training frame
-pxds = pixelLabelDatastore(strcat(newPath,'dataset/GT_TRAIN'),["N","B"],[0 1]); % link for GT images
+imds = imageDatastore(strcat(newPath,'dataset\FRAME_TRAIN')); % link for training frame
+pxds = pixelLabelDatastore(strcat(newPath,'dataset\GT_TRAIN'),["N","B"],[0 1]); % link for GT images
 
 %% 
 % RES-NET50
-numClasses=2; % problema binario ma si può impostare un problema a più classi
-imageSize=[256 256 3]; % dipende dalla rete che si usa
-% augmenter serve per ampliare il dataset di immagini e rispettivo GT con
-% modifiche rigide quali traslazioni, riflessioni
+net=resnet50;
+numClasses=2; % foreground and background
+imageSize=net.Layers(1).InputSize; %read size directly from net
+
 augmenter = imageDataAugmenter('RandRotation',[0 360],'RandXReflection',true,'RandXTranslation',[-10 10],'RandYTranslation',[-10 10]);
 pximds = pixelLabelImageDatastore(imds,pxds,'DataAugmentation',augmenter,'OutputSize',imageSize,'ColorPreprocessing','gray2rgb');
 
-
 lgraph = deeplabv3plusLayers(imageSize, numClasses, "resnet50"); 
-% queste blocco serve per bilanciare rispetto alle classi background e
-% foreground
+% balance predominance of 0
 tbl = countEachLabel(pximds);
 totalNumberOfPixels = sum(tbl.PixelCount);
 frequency = tbl.PixelCount / totalNumberOfPixels;
 classWeights = 1./frequency;
 pxLayer = pixelClassificationLayer('Name','labels','Classes',tbl.Name,'ClassWeights',classWeights);
 lgraph = replaceLayer(lgraph,"classification",pxLayer);
-
-
-%options = trainingOptions('sgdm', ...
-%    'MaxEpochs',30, ...  
-%    'MiniBatchSize',8, ...
-%    'Plots','training-progress');
-
-
 options = trainingOptions('sgdm', ...
     'MaxEpochs',30, ...  
     'MiniBatchSize',8, ...
     'Plots','training-progress');
 %% Train the network.
 % Train the network.
-% devo prevedere un resize dei crop per poterli dare in pasto alla rete
+[net, info]= trainNetwork(pximds,lgraph,options);
+save('myNet.mat','net','lgraph');
+subfigure(2,1,1);
+plot(1:length(info.TrainingAccuracy),info.TrainingAccuracy)
+title('Training Accuracy')
+subfigure(2,1,2);
+plot(info.TrainingLoss)
+title('Training Loss')
 
-net = trainNetwork(pximds,lgraph,options);
-% save('Net_CAF_25012020.mat','net','lgraph');
-%%
-%% test di tutti i frame 
-close all;
+%% test 
 f_test=dir(strcat(dataPath,'/FRAME_TEST_SEG/*.tiff'));
 gt_train=dir(strcat(dataPath,'/GT_TEST/*.tiff'));
 for l = 1:length(f_test)
@@ -64,9 +57,19 @@ testImage=imread([strcat(dataPath,'/FRAME_TEST_SEG/'),f_test(l).name]);
 C_test = semanticseg(testImage,net);
 D=C_test=='B';
 GTImage=imread([strcat(dataPath,'/GT_TEST/'),gt_train(l).name]);
-[TP(l),FP(l),FN(l),CR(l),CM(l),FM_test(l)]=evaluation_segmentation(bwareafilt(D,1),GTImage);
+[TP,FP,FN,CR,CM,FM_test(l)]=evaluation_segmentation(bwareafilt(D,1),GTImage);
 % imwrite(D,['FRAME_TEST_SEG_NEW\',f_test(l).name(1:end-5),'_seg.tiff']);
 imshowpair(testImage,bwareafilt(D,1),'montage');
 pause(0.5); drawnow;
 clear C_test D testImage;
 end
+figure;
+plot(FM_test);
+ylim([0 1]); title('FM')
+line([0 length(FM_test)],[mean(FM_test) mean(FM_test)],'Color','red','LineStyle','--');
+med=mean(FM_test)
+best=max(FM_test)
+worst=min(FM_test)
+sigma=std(FM_test)
+
+%% cycle over 

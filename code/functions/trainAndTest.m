098a660b9c9fe2b5ceb8a759d8e76ddae379b777
mastroalex <76497903+mastroalex@users.noreply.github.com>
2022-02-11 11:35:18 +0100

    update

--- code/functions/trainAndTest.m
+++ code/functions/trainAndTest.m
@@ -6,17 +6,27 @@ options = trainingOptions('sgdm', ...
     'Plots','training-progress');
 %Training 
 [net, info]= trainNetwork(pximds,lgraph,options);
-
+flag= false;
 %Test
 tic
 for l = 1:length(f_test)
 testImage=imread([strcat(dataPath,'/FRAME_TEST_SEG/'),f_test(l).name]);
+GTImage=imread([strcat(dataPath,'/GT_TEST/'),gt_train(l).name]);
+if net.Layers(1).InputSize>=[size(testImage) 3]
+    flag=true;
+    testImage=imresize(testImage,net.Layers(1).InputSize(1:2)+[1 1]);
+    GTImage=imresize(GTImage,net.Layers(1).InputSize(1:2)+[1 1]);
+end
 C_test = semanticseg(testImage,net);
 D=C_test=='B';
-GTImage=imread([strcat(dataPath,'/GT_TEST/'),gt_train(l).name]);
+
 [TP,FP,FN,CR,CM,FM_test(l)]=evaluation_segmentation(bwareafilt(D,1),GTImage);
 clear C_test D testImage;
 end
 compTime=toc;
+if flag==true
+    
+    disp('img scaled')
+end
 
 end
